1e02b632d1b4a1484597728c8eca624cb60f359b
StrokPavel <kastet2912@gmail.com>
2017-11-12 17:41:37 +0300

    fixed text-style

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function down() {
   var a = document.getElementById('dropdown');
   if (a.style.display == 'none')
-    a.style.display = 'block'
+    a.style.display = 'block';
   else
   if (a.style.display == 'block')
     a.style.display = 'none';
