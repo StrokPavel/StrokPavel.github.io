f41af31c816520072f944dac333f7c6b91aec7cf
StrokPavel <kastet2912@gmail.com>
2017-11-12 16:56:18 +0300

    add info about me

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function down() {
+  var a = document.getElementById('dropdown');
+  if (a.style.display == 'none')
+    a.style.display = 'block'
+  else
+  if (a.style.display == 'block')
+    a.style.display = 'none';
+};
