6afab687085b0f6d77fefb0344628174337e8125
StrokPavel <pavel.strok.97@yandex.ru>
2017-11-13 10:56:17 +0300

    update info about me

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ function down() {
   if (a.style.display == 'block')
     a.style.display = 'none';
 };
+
+var win = null;
+
+function NewWindow(mypage,myname,w,h,scroll){
+  var a = document.getElementById('NewWindow');
+LeftPosition = (screen.width) ? (screen.width-w)/2 : 0;
+TopPosition = (screen.height) ? (screen.height-h)/2 : 0;
+settings =
+'height='+h+',width='+w+',top='+TopPosition+',left='+LeftPosition+',scrollbars='+scroll+',resizable'
+win = window.open(mypage,myname,settings)
+}
